07874f3427a1f4c8bd7a2a0e63028b6ed2c3d27c
Anushil Raman <anushilraman1993@gmail.com>
2024-08-22 03:32:49 +0530

    Implemented the roles in front end

---
+++ FrontEnd/src/auth/AuthGuard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import useAuth from '../hooks/useAuth.hook'
+import AuthSpinner from '../componenets/general/authSpinner';
+import { Navigate, Outlet } from 'react-router-dom';
+import { PATH_PUBLIC } from '../routes/paths';
+
+
+interface IProps {
+    roles: string[]
+}
+const AuthGuard = ({ roles }: IProps) => {
+    const { isAuthenticated, user, isAuthLoading } = useAuth();
+    const hasAccess = isAuthenticated && user?.roles?.find((x) => roles.includes(x));
+    if (isAuthLoading) {
+        return <AuthSpinner />;
+    }
+    return hasAccess ? <Outlet /> : <Navigate to={PATH_PUBLIC.unauthorized} />;
+}
+export default AuthGuard
