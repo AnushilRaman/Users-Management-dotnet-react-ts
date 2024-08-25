5131766acde31d56e8e631711a103112c0582f4c
Anushil Raman <anushilraman1993@gmail.com>
2024-08-26 03:43:25 +0530

    Layout and Side bar

---
+++ FrontEnd/src/componenets/layout/Index.tsx
@@ -0,0 +1,31 @@
+import useAuth from "../../hooks/useAuth.hook";
+import Header from "./Header";
+import { Outlet, useLocation } from "react-router-dom";
+import SideBar from "./SideBar";
+
+const Layout = () => {
+    const { isAuthenticated } = useAuth();
+    const { pathname } = useLocation();
+
+    console.log(pathname);
+
+    const sideBarRenderer = () => {
+        if (isAuthenticated && pathname.toLowerCase().startsWith('/dashboard')) {
+            return <SideBar />
+        }
+        return null;
+    }
+
+    return (
+        <div>
+            <Header />
+            {/* Using Outlet, we render all routes that are inside of this Layout  */}
+            <div className="flex">
+                {sideBarRenderer()}
+                <Outlet />
+            </div>
+        </div>
+    )
+}
+
+export default Layout
