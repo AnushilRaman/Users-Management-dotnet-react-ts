c1d8b8b8237cf8152dd042f6e047ceaf93a0eab4
Anushil Raman <anushilraman1993@gmail.com>
2024-08-30 04:55:59 +0530

    Adding frontend Message functionality

--- FrontEnd/src/pages/dashboard/AllMessagePage.tsx
+++ FrontEnd/src/pages/dashboard/AllMessagePage.tsx
@@ -1,7 +1,60 @@
+import { useEffect, useState } from "react";
+import { IMessageDto } from "../../types/message.types";
+import axiosInstance from "../../utils/axiosInstance";
+import { ALL_MESSAGES_URL } from "../../utils/globalConfig";
+import { toast } from "react-hot-toast";
+import Spinner from "../../componenets/general/Spinner";
+import moment from "moment";
+import { MdInput, MdOutput } from "react-icons/md";
+import useAuth from "../../hooks/useAuth.hook";
+
 const AllMessagePage = () => {
+  const [messages, setMessages] = useState<IMessageDto[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+
+  const getAllMessage = async () => {
+    try {
+      setLoading(true);
+      const response = await axiosInstance.get<IMessageDto[]>(ALL_MESSAGES_URL);
+      const { data }: any = response;
+      setMessages(data);
+      setLoading(false);
+    } catch (error) {
+      toast.error('An Error happened. Please Contact admins');
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    getAllMessage();
+  }, [])
+
+  if (loading) {
+    return <div className="w-full">
+      <Spinner />
+    </div>
+  }
   return (
-    <div>
-      
+    <div className="pageTemplate2">
+      <h1 className="text-2xl font-bold">All Messages</h1>
+      <div className="pageTemplate3 items-stretch">
+        <div className="grid grid-cols-6 p-2 border-2 border-gray-200 rounded-lg">
+          <span>Date</span>
+          <span className="col-spna-4">Text</span>
+          <span>Sender</span>
+          <span>Receiver</span>
+        </div>
+        {
+          messages.map((item) => (
+            <div key={item.id} className="grid grid-cols-6 p-2 border-2 border-gray-200 rounded-lg">
+              <span>{moment(item.createdAt).fromNow()}</span>
+              <span className="col-spna-4">{item.text}</span>
+              <span>{item.senderUserName}</span>
+              <span>{item.receiverUserName}</span>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
