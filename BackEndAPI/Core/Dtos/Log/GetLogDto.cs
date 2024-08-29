7524d1d390aa474d00ade83da4e82b4c5eab696b
Anushil Raman <anushilraman1993@gmail.com>
2024-08-30 04:41:29 +0530

    Adding new Dto

---
+++ BackEndAPI/Core/Dtos/Log/GetLogDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BackEndAPI.Core.Dtos.Log
+{
+    public class GetLogDto
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string? UserName { get; set; }
+        public string? Description { get; set; }
+    }
+}
